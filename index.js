3721a87057ee1b19462de03ec218867a71e14dd0
fanta <248224524@qq.com>
2022-07-17 20:21:14 +0800

    build

---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+let fs = require('fs');
+const path = require('path');
+const cwd = process.cwd();
+const exts = ['.mp4', '.mkv','.avi','.rmvb','.mov','.m4v','.wmv','.asf','.asx','.3gp','.flv'];
+const root = cwd;
+
+
+function rename(file){
+  const match = file.match(/S(\d+)E(\d+)/);
+  const ext = path.extname(file);
+  const newName= `${match[0]}${ext}`;
+  fs.renameSync(file,newName,(err)=>{
+    if(err){
+      console.log('出错')
+    }else{
+      console.log(newName)
+    }
+  })
+}
+function fileFilter(file) {
+  fs.stat(file, (err, stats) => {
+    if (err) return console.error(err);
+    if (stats.isFile() && exts.includes(path.extname(file))) {
+      rename(file); // console.log('可以压缩：' + file);
+    }
+  });
+}
+
+// 获取文件列表
+function fileList(folder) {
+  fs.readdir(folder, (err, files) => {
+    if (err) console.error(err);
+    files.forEach(file => {
+      fileFilter(file);
+    });
+  });
+}
+
+fileList(root);
---
+++ readme.md
@@ -0,0 +1,12 @@
+
+## 用途
+
+
+## 安装
+
+npm i rename-video -g
+
+
+## usage
+1. 定位到你需要处理的视频文件目录`cd yourVideoFilePath`
+2. 执行 `rename-video`
